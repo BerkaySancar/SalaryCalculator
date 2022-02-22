63e750249a79ed268fb386755387aa897a3a564f
Berkay Sancar <sancarberkay@gmail.com>
2022-02-22 18:43:33 +0300

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,58 @@
+public class Employee {
+
+    String name;
+    int salary;
+    int workHours;
+    int hireYear;
+
+    Employee(String name, int salary, int workHours, int hireYear) {
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+    }
+
+    double tax() {
+        if (this.salary < 1000) {
+            return 0;
+        } else {
+            return this.salary * 0.03;
+        }
+    }
+
+    double bonus() {
+        int sum, total = 0;
+        if (this.workHours > 40)
+            for (int i = 41; i <= this.workHours; i++) {
+                sum = i % 10;
+                total = sum;
+            }
+        return total * 30;
+    }
+
+    double raiseSalary() {
+        double tempSalary = this.salary;
+        int totalYear = 2021 - this.hireYear;
+        if (totalYear < 10) {
+            return tempSalary * 0.05;
+        } else if (totalYear < 20) {
+            return tempSalary * 0.10;
+        } else {
+            return tempSalary * 0.15;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return  "Name: " + this.name + "\n" +
+                "Salary: " + this.salary + "\n" +
+                "Work Hours: " + this.workHours + "\n" +
+                "Hire Year: " + this.hireYear + "\n" +
+                "Tax: " + tax() + "\n" +
+                "Bonus: " + bonus() + "\n" +
+                "Salary increase: " + raiseSalary() + "\n" +
+                "Salary with tax and bonus: " + (this.salary - tax() + bonus()) + "\n" +
+                "Total Salary: " + (this.salary + raiseSalary() - tax() + bonus());
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Sebastian Vettel",2000,45,1985);
+        Employee e2 = new Employee("Lewis Hamilton",900,39,2001);
+
+        System.out.println(e1);
+        System.out.println(e2);
+
+    }
+}
